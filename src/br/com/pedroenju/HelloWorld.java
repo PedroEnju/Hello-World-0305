0054ed1a1c7969bc5df7105088be82a4bef779a5
Pedro Enju <pedro_enju@hotmail.com>
2018-05-10 20:40:27 -0300

    JOptionPane

--- src/br/com/pedroenju/HelloWorld.java
+++ src/br/com/pedroenju/HelloWorld.java
@@ -1,6 +1,7 @@
 package br.com.pedroenju;
 
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class HelloWorld {
 
@@ -8,8 +9,29 @@ public class HelloWorld {
         int numero = 0;
         Random ran = new Random();
         int meuIntAleatorio = ran.nextInt(100);
-       
+
         System.out.println("Hello World!!!");
+
+//        JOptionPane.showMessageDialog(null, "Hello World! \r\nDefault");
+//        JOptionPane.showMessageDialog(null, "Hello World!! \r\nQuestion", "Question Message", JOptionPane.QUESTION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello World!!! \r\nError", "Error Message", JOptionPane.ERROR_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello World!!!! \r\nWarning", "Warning Message", JOptionPane.WARNING_MESSAGE);
+//        String entrada = JOptionPane.showInputDialog("Digite alguma coisa");
+//        JOptionPane.showMessageDialog(null, entrada);
+        try {
+            int i = Integer.parseInt(JOptionPane.showInputDialog("Digite um inteiro"));
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null, "Houve algum problema!");
+        }
+
+        try {
+            float j = Float.parseFloat(JOptionPane.showInputDialog("Digite um flutuante"));
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null, "Houve algum problema!");
+        }
+
+//        String k = Integer.toString(i);
+//        String l = Float.toString(j);
     }
 
 }
